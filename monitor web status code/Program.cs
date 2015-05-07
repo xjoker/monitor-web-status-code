c2179663cdea06a4a81d6a978d0671ac1a9d3833
xJoker <xjoker@xjoker.us>
2015-05-07 17:17:21 +0800

    更新了状态码的检测过程

--- monitor web status code/Program.cs
+++ monitor web status code/Program.cs
@@ -7,13 +7,16 @@ namespace monitor_web_status_code
     {
         static void Main(string[] args)
         {
-            string[] parameters = new string[2];
+
+
+            string[] parameters = new string[3];
             if (args.Length != 0)
             {
                 parameters = args;
 
                 if (parameters.Length != 0)
                 {
+                    #region web.dis功能
                     //参数为“web_site_discovery”时
                     if (parameters[0] == "web.dis")
                     {
@@ -24,11 +27,11 @@ namespace monitor_web_status_code
                         int a = 0;
                         foreach (var item in aaa)
                         {
-                            
+
                             Console.WriteLine("\t\t{");
                             Console.Write("\t\t\t\"{#SITENAME}\":\"" + item + "\"}");
                             a++;
-                            if(aaa.Length==a)
+                            if (aaa.Length == a)
                             {
                                 Console.Write("]}");
                             }
@@ -37,42 +40,63 @@ namespace monitor_web_status_code
                                 Console.Write(",\n");
                             }
                         }
-                    }
+                    } 
+                    #endregion
 
-                    //网站状态码
+
+                    #region 网站状态码
+
+                    int code;
                     if (parameters[0] == "web.code" && parameters[1] != null)
                     {
-                        if(parameters[2]==null)
+
+
+
+
+
+
+                        if (parameters.Length>2)
                         {
                             try
                             {
-
                                 web_site_code wsc = new web_site_code();
-                                int code = wsc.Get_code(parameters[1]);
-                                Console.Write(code);
-                            }
-                            catch
-                            {
-
-                                Console.WriteLine("Parameters error!");
-                                Console.WriteLine("URL typing errors");
-                            }  
-                        }
-                        else
-                        {
-                            try
-                            {
-
-                                web_site_code wsc = new web_site_code();
-                                int code = wsc.Get_code(parameters[1],parameters[2]);
+                                code = wsc.Get_code(parameters[1], parameters[2]);
                                 Console.Write(code);
                             }
                             catch
                             {
                                 Console.Write("404");
-                            }  
+                            }
                         }
-                    }
+                        else
+                        {
+                            
+                            try
+                            {
+
+                                web_site_code wsc = new web_site_code();
+
+                                if(parameters[1].Substring(0,1 )=="$")
+                                {
+                                    //遇到带 "$" 的处理方法
+                                    code = wsc.Get_code(parameters[1].Substring(1,parameters[1].Length-1));
+                                }
+                                else{
+                                    //一般模式检测IP地址127.0.0.1
+                                    code = wsc.Get_code(parameters[1].ToString()+"/zabbix/zabbix.aspx","127.0.0.1");
+                                }
+
+                                
+                                Console.Write(code);
+                            }
+                            catch //(Exception ex)
+                            {
+                                //Console.WriteLine(ex);
+                                Console.WriteLine("404");
+                            }
+                        }
+                    } 
+                    #endregion
                 }
             }
             else
--- monitor web status code/web_site_code.cs
+++ monitor web status code/web_site_code.cs
@@ -8,42 +8,58 @@ namespace monitor_web_status_code
     {
         public int Get_code(string url, string ip = "NOIP")
         {
-            string test_ip = string.Empty; //要测试的IP
 
+                string test_ip = string.Empty; //要测试的IP
 
-            Uri u = new Uri(url);
-            url = u.Host;
+                url = "http://" + url;
+                Uri u = new Uri(url);
+                url = u.Host;
+                //判断是否传入了IP
+                if (ip != "NOIP")
+                {
+                    //使用IP直接访问站点
+                    test_ip = "http://" + ip;
 
-            //判断是否传入了IP
-            if (ip != "NOIP")
-            {
-                //使用IP直接访问站点
-                test_ip = "http://" + ip;
+                }
+                else
+                {
+                    test_ip = u.AbsoluteUri;
+                    
+                }
+                HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(test_ip);
 
-            }
-            else
-            {
-                test_ip = u.AbsoluteUri;
-            }
-            HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(test_ip);
+                hwr.KeepAlive = false;
+                
+                hwr.Host = url;
 
-            hwr.Host = url;
+                hwr.Method = "GET";
 
-            hwr.Method = "GET";
+                hwr.Timeout = 2000; //1秒超时
 
-            int http_code;
+                HttpWebResponse hwrs = (HttpWebResponse)hwr.GetResponse();
 
-            HttpWebResponse hwrs = (HttpWebResponse)hwr.GetResponse();
+                switch (hwrs.StatusCode)
+                {
+                    
+                    case HttpStatusCode.InternalServerError:
+                        return 500;
 
-            http_code = (int)hwrs.StatusCode;//获得状态编码
-            return http_code;
+                    case HttpStatusCode.Moved:
+                        return 301;
 
-
-
-
-
-
-
+                    case HttpStatusCode.OK:
+                        return 200;
+                       
+                    case HttpStatusCode.RequestTimeout:
+                        return 408;
+                        
+                    case HttpStatusCode.ServiceUnavailable:
+                        return 503;
+                        
+                    default:
+                        return 0000;
+                        
+                }
         }
     }
 }
